922c5603b64e0cbeb142910e58c2a76577b2343f
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-08-06 00:10:54 +0530

    Resolved CORS

--- app/backend/order/route.js
+++ app/backend/order/route.js
@@ -110,7 +110,7 @@ export async function POST(req) {
             },
             { status: 201,
                 headers: {
-                     "Access-Control-Allow-Origin": "http://localhost",
+                     "Access-Control-Allow-Origin": "https://payal-fruits.in",
             "Access-Control-Allow-Methods": "POST, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type"
                 }
